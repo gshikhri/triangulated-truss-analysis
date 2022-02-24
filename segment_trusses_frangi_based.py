c920556d332643328bbf5ad55f65c2a8111db855
gshikhri <gshikhri@gmail.com>
2022-02-24 11:36:07 +0100

    Filter trusses based on bound_box dimensions

--- segment_trusses_frangi_based.py
+++ segment_trusses_frangi_based.py
@@ -1,49 +1,55 @@
+from cProfile import label
 import os
 import matplotlib.pyplot as plt
 import matplotlib
-from skimage import io, filters, morphology, measure
+from skimage import io, filters, morphology, measure, feature, future, segmentation, util
 from scipy import ndimage
 from skimage.color import label2rgb
 import os
+import numpy as np
+import pandas as pd
+from functools import partial
+from sklearn.ensemble import RandomForestClassifier
+from scipy import ndimage
 
 matplotlib.use('Qt5Agg')
 
 in_folder = 'wireframe-tubes'
 
 for _, _, filenames in os.walk(in_folder):
-    for file in filenames[:]:
-        truss_image = io.imread(os.path.join(in_folder, file))[128:640, 128:640]
+    for file in filenames[-2:-1]:
+        truss_image = io.imread(os.path.join(in_folder, file))
 
         thresh = filters.threshold_otsu(truss_image)
         binary = truss_image > thresh
         morphed_image = morphology.remove_small_objects(morphology.remove_small_holes(binary))
+
         edges = filters.frangi(morphed_image)
-        dilated_edges = morphology.binary_dilation(edges)
+        eroded_edges = morphology.binary_erosion(edges, np.ones(shape=(10, 10)))
 
-        labelled_image = measure.label(dilated_edges)
+        labelled_image = measure.label(eroded_edges)
 
-        fig, ax = plt.subplots(nrows=2, ncols=2, sharex=True, sharey=True)
-        ax[0][0].imshow(truss_image)
-        ax[0][1].imshow(morphed_image)
-        ax[1][0].imshow(edges)
-        ax[1][1].imshow(dilated_edges)
-        ax[1][1].imshow(label2rgb(labelled_image, image=truss_image, bg_label=0))
+        regions = measure.regionprops_table(label_image=labelled_image, \
+            intensity_image=truss_image, properties=('label', 'area'))
+        df = pd.DataFrame(regions)
+        outlier_range = np.quantile(df['area'], [0, 1])
+        bad_labels = df[~df['area'].between(*outlier_range)].dropna()['label'].to_numpy().astype(np.int16)
+        for i in list(bad_labels):
+            labelled_image = np.where(labelled_image == i, 0, labelled_image)
+        
+        binary_labelled = np.where(labelled_image != 0, 1, 0)
+        
+        fig, ax = plt.subplots(nrows=1, ncols=3, sharex=True, sharey=True, figsize=(10, 8))
+        ax[0].imshow(truss_image)
+        ax[1].imshow(binary_labelled)
+        ax[2].imshow(label2rgb(labelled_image, image=truss_image, bg_label=0))
         fig.suptitle(file)
+        fig.tight_layout()
         plt.show()
 
+        # fig, ax = plt.subplots()
+        # ax.imshow(label2rgb(morphology.skeletonize(binary_labelled), image=truss_image, bg_label=0))
+        # fig.suptitle(file)
+        # plt.show()
 
-
-# fig, ax = plt.subplots(nrows=1, ncols=3, sharex=True, sharey=True)
-# ax[0].imshow(labelled_image)
-
-# labelled_image[labelled_image <=1] = 0
-# labelled_image[labelled_image >=2] = 1
-
-# ax[1].imshow(labelled_image)
-
-# ax[2].imshow(morphology.skeletonize(labelled_image))
-
-# plt.show()
-
-# io.imsave(os.path.splitext(file_name)[0] + str('_skeleton.tif'), morphology.skeletonize(labelled_image))
 a = 2
